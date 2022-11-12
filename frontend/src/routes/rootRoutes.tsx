518fe520dc4f27645e702e217fcdd893fcd0d0b4
abhishek <abhiskg20@gmail.com>
2022-11-12 21:39:17 +0600

    Header added

--- frontend/src/routes/rootRoutes.tsx
+++ frontend/src/routes/rootRoutes.tsx
@@ -1,10 +1,32 @@
 import { createBrowserRouter } from "react-router-dom";
 import Root from "../layout/Root";
+import Appointment from "../pages/appointment/Appointmentz";
+import Login from "../pages/auth/Login";
+import Register from "../pages/auth/Register";
+import Home from "../pages/home/Home";
 
 const rootRoutes = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/register",
+        element: <Register />,
+      },
+      {
+        path: "/appointment",
+        element: <Appointment />,
+      },
+    ],
   },
 ]);
 
--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          50: "#0FCFEC",
+          100: "#19D3AE",
+        },
+        secondary: "#3A4256",
+      },
+    },
   },
   plugins: [],
 };
