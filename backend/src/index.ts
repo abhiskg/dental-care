e11d449bcc4fdddc4f71d6d8ffc846dcdc7843f9
abhishek <abhiskg20@gmail.com>
2022-11-18 14:30:05 +0600

    Initial express server with mongodb connected

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import dbConnect from "./config/dbConnect";
 
 const app = express();
 
@@ -7,3 +8,13 @@ app.use(cors());
 app.use(express.json());
 
 const port = process.env.PORT || 5000;
+
+dbConnect()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Database Connected & Running on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log((err as Error).message);
+  });
