f7f170c13d04b685f3f91d53cdb2695a3165ea05
abhishek <abhiskg20@gmail.com>
2022-11-18 11:55:41 +0600

    Authcontext added

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,9 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import "react-day-picker/dist/style.css";
+import AuthContextProvider from "./context/AuthContext";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <AuthContextProvider>
+      <App />
+    </AuthContextProvider>
   </React.StrictMode>
 );
